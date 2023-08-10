ee7ed219397d3d9bcdc6bd5f67d0086d36b8eb16
erluzi <747877078@qq.com>
2023-08-10 14:36:44 +0800

    feat: move file

--- shared/u.js
+++ shared/u.js
@@ -4,3 +4,26 @@ function print(options, el = document.body) {
   appendEl.textContent = message
   el.appendChild(appendEl)
 }
+
+function getRunner() {
+  let lastTime = 0
+  let id = null
+  function runner(callback, t) {
+    let currTime = new Date().getTime()
+    let timeWait = Math.max(0, t - (currTime - lastTime))
+    id = window.setTimeout(() => {
+      callback()
+      runner(callback, t)
+    }, timeWait)
+    lastTime = currTime + timeWait
+  }
+  return {
+    run(callback, t) {
+      if (!id) runner(callback, t)
+    },
+    destroy() {
+      window.clearTimeout(id)
+      id = null
+    }
+  }
+}
